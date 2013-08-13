28bedf49fe850119eee575954aab0775f5d95eaf
eiji.ienaga <eiji.ienaga@gmail.com>
2013-08-13 16:38:05 +0900

    tab to space

--- src/main/java/example/HelloServlet.java
+++ src/main/java/example/HelloServlet.java
@@ -9,13 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello")
-public class HelloServlet extends HttpServlet
-{
-	private static final long serialVersionUID = 1L;
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-    		throws ServletException, IOException {
-    	response.getWriter().println("Hello World!");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.getWriter().println("Hello World!");
     }
 }
